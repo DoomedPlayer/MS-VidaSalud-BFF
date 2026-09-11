519268df9b7ef3d1d7e249b499da8f0d5c40a607
DoomedPlayer <ni.beeger@duocuc.cl>
2026-09-11 17:43:24 -0300

    Implementacion BFF con OpenFeign

--- src/main/java/com/BinarySeint/vsBFF/VsBffApplication.java
+++ src/main/java/com/BinarySeint/vsBFF/VsBffApplication.java
@@ -2,8 +2,10 @@ package com.BinarySeint.vsBFF;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients 
 public class VsBffApplication {
 
 	public static void main(String[] args) {
