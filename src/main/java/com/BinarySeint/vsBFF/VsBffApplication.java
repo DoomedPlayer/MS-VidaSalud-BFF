e62f97f5f3b0a63433aa01a706cf5c20a14a5406
DoomedPlayer <ni.beeger@duocuc.cl>
2026-09-11 17:26:02 -0300

    Creación BFF

---
+++ src/main/java/com/BinarySeint/vsBFF/VsBffApplication.java
@@ -0,0 +1,13 @@
+package com.BinarySeint.vsBFF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsBffApplication.class, args);
+	}
+
+}
